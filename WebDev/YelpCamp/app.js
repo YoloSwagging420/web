dbbd3c5dba6051b41bf4c058d349c89262177b84
YoloSwagging420 <yaoyixuan1992@gmail.com>
2021-07-23 22:24:31 -0400

    123

--- WebDev/YelpCamp/app.js
+++ WebDev/YelpCamp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campgrounds');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -17,6 +18,8 @@ db.once("open", () => {
 });
 
 const app = express();
+
+app.engine('ejs', ejsMate);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
--- WebDev/YelpCamp/seeds/index.js
+++ WebDev/YelpCamp/seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const prices = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251/800x450',
+            descripton: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore deserunt ipsa adipisci quisquam reprehenderit laborum illum qui voluptatibus beatae. Perferendis eveniet omnis dolorum soluta dignissimos, optio eligendi inventore. Delectus.',
+            price: prices,
         })
         await camp.save();
     }
