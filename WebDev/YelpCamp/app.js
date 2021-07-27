555caad00c2b3e0899d755d465f049a9088e2f5b
YoloSwagging420 <yaoyixuan1992@gmail.com>
2021-07-26 22:27:57 -0400

    123

--- WebDev/YelpCamp/app.js
+++ WebDev/YelpCamp/app.js
@@ -109,7 +109,7 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
 app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(req.params.reviewId);
+    await Review.findByIdAndDelete(reviewId);
     res.redirect(`/campgrounds/${id}`);
 }))
 
